017b3723a26c5b3379414d25efc0960702c19688
André Rainho <arainho.it@gmail.com>
2021-12-05 01:27:48 +0000

    Update enumerate_endpoints.sh

--- drafts/enumerate_endpoints.sh
+++ drafts/enumerate_endpoints.sh
@@ -6,19 +6,14 @@
 TARGET_TLD="$1"
 WORDLIST="$2"
 
-go install github.com/OJ/gobuster/v3@latest
-~/go/bin/gobuster -e -u http://${TARGET_TLD} -w ${WORDLIST} -o gobuster-${TARGET_TLD}.output
-
-go get -u -v github.com/ffuf/ffuf
-~/go/bin/ffuf -w ${WORDLIST} -u https://${TARGET_TLD}/FUZZ -mc all -c -v -o ffuf-${TARGET_TLD}.output
-
-curl -L -o config.ini https://raw.githubusercontent.com/OWASP/Amass/master/examples/config.ini
-amass enum -active -d ${TARGET_TLD} -config config.ini
-
-git clone --depth=1 https://github.com/projectdiscovery/nuclei-templates.git
-nuclei -target ${TARGET_TLD} -t nuclei-templates/exposures/apis/
-
-## In progress
+gobuster -e -u http://${TARGET_TLD} -w ${WORDLIST} -o gobuster-${TARGET_TLD}.output
+ffuf -w ${WORDLIST} -u https://${TARGET_TLD}/FUZZ -mc all -c -v -o ffuf-${TARGET_TLD}.output
+git clone --depth=1 https://github.com/projectdiscovery/nuclei-templates.git && \
+  nuclei -target ${TARGET_TLD} -t nuclei-templates/exposures/apis/
+  
+# in progress
+# curl -L -o config.ini https://raw.githubusercontent.com/OWASP/Amass/master/examples/config.ini && \
+  amass enum -active -d ${TARGET_TLD} -config config.ini
 # jaeles scan -s swagger-ui-probing.yaml -u ${TARGET_TLD}
 # arjun -u https://${TARGET_TLD}/endpoint
 # python3 paramspider.py --domain ${TARGET_TLD}
